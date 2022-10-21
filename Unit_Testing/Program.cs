6d9316853c373e6374f77a7a3b23f6f4bb0dc2b7
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-22 00:28:50 +0530

    Gregorian Calender Program

--- Unit_Testing/Program.cs
+++ Unit_Testing/Program.cs
@@ -4,28 +4,87 @@
     {
         static void Main(string[] args)
         {
-            int Far = 0;
-            int cel = 1;
-            Console.WriteLine("Enter zero(0) to convert to celsius from farenheit and one(1) for vice versa  ");
-            int check = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Welcome to Gregorian Calender");
+            Console.WriteLine("Enter Month");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Day");
+            double d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Year");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(y - (14 - m / 12));
+            double x = y0 + y0 / 4 + y0 / 400 - y0 / 100;
+            double m0 = m + (12 * (14 - m) / 12) - 2;
+            double d0 = (int)(d + x + 31 * m / 12) % 12;
+            switch (m)
+            {
+                case 1:
+                    Console.WriteLine("January");
+                    break;
+                case 2:
+                    Console.WriteLine("February");
+                    break;
+                case 3:
+                    Console.WriteLine("March");
+                    break;
+                case 4:
+                    Console.WriteLine("April");
+                    break;
+                case 5:
+                    Console.WriteLine("May");
+                    break;
+                case 6:
+                    Console.WriteLine("June");
+                    break;
+                case 7:
+                    Console.WriteLine("July");
+                    break;
+                case 8:
+                    Console.WriteLine("August");
+                    break;
+                case 9:
+                    Console.WriteLine("September");
+                    break;
+                case 10:
+                    Console.WriteLine("October");
+                    break;
+                case 11:
+                    Console.WriteLine("November");
+                    break;
+                case 12:
+                    Console.WriteLine("December");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
 
-            if (check == 0)
-            {
-                Console.WriteLine("Enter the Temprature in Fahrenheit");
-                double F = Convert.ToDouble(Console.ReadLine());
-                double fc = Convert.ToDouble((F - 32) * 5 / 9);
-                Console.WriteLine($"{fc} Degree Celsius");
             }
-            else if (check == 1)
+            switch (d0)
             {
-                Console.WriteLine("Enter the Temprature in Celsius");
-                double C = Convert.ToDouble(Console.ReadLine());
-                double cf = Convert.ToDouble((C * 9 / 5) + 32);
-                Console.WriteLine($"{cf} Degree Farenheit");
-            }
-            else
-            {
-                Console.WriteLine("You have Entered A INVALID Input");
+
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("invallid input given");
+                    break;
             }
         }
     }
