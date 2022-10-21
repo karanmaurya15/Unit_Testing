d42d024a7b97b933bd8e2f7469f7d703eb38e190
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-22 00:20:04 +0530

    Temprature Conversion Program

--- Unit_Testing/Program.cs
+++ Unit_Testing/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int Far = 0;
+            int cel = 1;
+            Console.WriteLine("Enter zero(0) to convert to celsius from farenheit and one(1) for vice versa  ");
+            int check = Convert.ToInt32(Console.ReadLine());
+
+            if (check == 0)
+            {
+                Console.WriteLine("Enter the Temprature in Fahrenheit");
+                double F = Convert.ToDouble(Console.ReadLine());
+                double fc = Convert.ToDouble((F - 32) * 5 / 9);
+                Console.WriteLine($"{fc} Degree Celsius");
+            }
+            else if (check == 1)
+            {
+                Console.WriteLine("Enter the Temprature in Celsius");
+                double C = Convert.ToDouble(Console.ReadLine());
+                double cf = Convert.ToDouble((C * 9 / 5) + 32);
+                Console.WriteLine($"{cf} Degree Farenheit");
+            }
+            else
+            {
+                Console.WriteLine("You have Entered A INVALID Input");
+            }
         }
     }
 }
